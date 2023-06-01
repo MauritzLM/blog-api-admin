836b1c730c22a0a4f74c43f2726ba31e818e444f
Mauritz Marais <mauritz303@gmail.com>
2023-06-01 17:50:18 +0200

    view posts component

---
+++ src/components/posts.jsx
@@ -0,0 +1,11 @@
+
+export default function Posts() {
+    // logged in?*
+    return (
+        <>
+            <h2>Posts</h2>
+            <p>Create New</p>
+            <p>Edit</p>
+        </>
+    )
+}
