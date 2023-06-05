9295d5c380b705a59d69f404c3259befd3135780
Mauritz Marais <mauritz303@gmail.com>
2023-06-05 16:14:38 +0200

    added error msg state and displaying correct form errors

--- src/components/sign-up.jsx
+++ src/components/sign-up.jsx
@@ -1,11 +1,24 @@
 import { useState } from "react"
+import { Link } from "react-router-dom";
 
 export default function Signup() {
+    // form inputs
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
     const [email, setEmail] = useState('');
     const [adminCode, setAdminCode] = useState('');
-    // user logged in?*
+
+    // error messages
+    const [usernameError, setUsernameError] = useState('');
+    const [passwordError, setPasswordError] = useState('');
+    const [emailError, setEmailError] = useState('');
+    const [adminCodeError, setAdminCodeError] = useState('');
+
+    // email Regex
+    const emailRegex = new RegExp(/^[A-Za-z0-9_!#$%&'*+/=?`{|}~^.-]+@[A-Za-z0-9.-]+$/, "gm");
+
+    const [adminCreated, setAdminCreated] = useState(false);
+    // user already logged in?*
 
     // handle form submit
     async function handleSubmit(event) {
@@ -19,52 +32,87 @@ export default function Signup() {
 
             const response = await fetch('http://localhost:3001/admin/signup', {
                 method: form.method, body: formData
-            })
+            });
             const data = await response.json();
 
             // if validation errors
             if (data.errors) {
                 data.errors.forEach(err => {
-                    console.log(err.msg);
+                    // display errors
+                    if (err.path === 'username') {
+                        setUsernameError(err.msg);
+                    }
+                    if (err.path === 'password') {
+                        setPasswordError(err.msg);
+                    }
+                    if (err.path === 'email') {
+                        setEmailError(err.msg);
+                    }
+                    if (err.path === 'admincode') {
+                        setAdminCodeError(err.msg);
+                    }
+
                 });
             } else {
-                console.log("admin created!")
+                // display result* (change state if succesful / admin created, link to login page)
+                setAdminCreated(true);
             }
         }
         catch (error) {
-            console.log(error)
+            console.log(error);
         }
     }
+
+    if (adminCreated) {
+        return (
+            <AdminCreated />
+        )
+    } else {
+        return (
+            <>
+                <h2>Sign up form</h2>
+                <form method="post" action="http://localhost:3001/admin/signup" onSubmit={(e) => handleSubmit(e)} noValidate>
+                    <div>
+                        <label htmlFor="username">username
+                            <input type="text" name="username" id="username" value={username} onChange={(e) => { setUsername(e.target.value) }} required></input>
+                            <span className="error">{username === '' ? usernameError : ''}</span>
+                        </label>
+                    </div>
+
+                    <div>
+                        <label htmlFor="password">password
+                            <input type="password" name="password" id="password" value={password} onChange={(e) => { setPassword(e.target.value) }} required></input>
+                            <span className="error">{password === '' || password.length < 8 ? passwordError : ''}</span>
+                        </label>
+                    </div>
+
+                    <div>
+                        <label htmlFor="email">email
+                            <input type="email" name="email" id="email" value={email} onChange={(e) => { setEmail(e.target.value) }} required></input>
+                            <span className="error">{emailRegex.test(email) ? '' : emailError}</span>
+                        </label>
+                    </div>
+
+                    <div>
+                        <label htmlFor="admincode">enter admin code
+                            <input type="password" name="admincode" id="admincode" value={adminCode} onChange={(e) => { setAdminCode(e.target.value) }} required></input>
+                            <span className="error">{adminCodeError}</span>
+                        </label>
+                    </div>
+
+                    <button type="submit" >Sign up</button>
+                </form>
+            </>
+        )
+    }
+}
+
+// successfully created admin component
+function AdminCreated() {
     return (
         <>
-            <h2>Sign up form</h2>
-            <form method="post" action="http://localhost:3001/admin/signup" onSubmit={(e) => handleSubmit(e)}>
-                <div>
-                    <label htmlFor="username">username
-                        <input type="text" name="username" id="username" value={username} onChange={(e) => { setUsername(e.target.value) }} required></input>
-                    </label>
-                </div>
-
-                <div>
-                    <label htmlFor="password">password
-                        <input type="password" name="password" id="password" value={password} onChange={(e) => { setPassword(e.target.value) }} required></input>
-                    </label>
-                </div>
-
-                <div>
-                    <label htmlFor="email">email
-                        <input type="email" name="email" id="email" value={email} onChange={(e) => { setEmail(e.target.value) }} required></input>
-                    </label>
-                </div>
-
-                <div>
-                    <label htmlFor="admincode">enter admin code
-                        <input type="password" name="admincode" id="admincode" value={adminCode} onChange={(e) => { setAdminCode(e.target.value) }} required></input>
-                    </label>
-                </div>
-
-                <button type="submit" >Sign up</button>
-            </form>
+            <h2>Admin Created!</h2>
+            <Link to='/login'>login</Link>
         </>
     )
 }
