4ab0bf18025d86fc859408e50b67b04ff1748af3
Mauritz Marais <mauritz303@gmail.com>
2023-06-07 16:28:07 +0200

    added loginstatus state and successful login

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,14 +1,13 @@
 import { useState } from "react"
-import Cookies from "js-cookie";
 
-export default function Login() {
+export default function Login({ isAuthenticated, handleAuthenticated }) {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
 
     // errors
     const [loginStatus, setLoginStatus] = useState('');
 
-
+    // handle login 
     async function handleLogin(event) {
         event.preventDefault();
 
@@ -18,47 +17,56 @@ export default function Login() {
 
         try {
             const response = await fetch('http://localhost:3001/admin/login', {
-                method: form.method, body: formData
+                method: form.method,
+                body: formData,
             });
 
             const data = await response.json();
 
-            // pass token as header to access secure routes*
-            // const token = Cookies.get('token');
+            setLoginStatus(data.msg)
 
-            // update logged in state* 
-            console.log(Cookies.get('token'));
+            // successfully logged in
+            if (data.authenticated) {
+                handleAuthenticated();
 
-            setLoginStatus(data.msg);
+                // redirect to homepage*
+                return;
+            }
         }
         catch (error) {
             console.log(error);
         }
     }
-    // already logged in?*
+    // already logged in?
 
-    // login post request
+    if (isAuthenticated) {
+        return (
+            <>
+                <h2>You are logged in!</h2>
+            </>
+        )
+    } else {
+        return (
+            <>
+                <h2>Login Form</h2>
+                <form method="post" onSubmit={(e) => handleLogin(e)} noValidate>
+                    <div>
+                        <label htmlFor="username">username
+                            <input type="text" name="username" id="username" value={username} onChange={(e) => setUsername(e.target.value)} required></input>
+                        </label>
+                    </div>
 
-    return (
-        <>
-            <h2>Login Form</h2>
-            <form method="post" onSubmit={(e) => handleLogin(e)} noValidate>
-                <div>
-                    <label htmlFor="username">username
-                        <input type="text" name="username" id="username" value={username} onChange={(e) => setUsername(e.target.value)} required></input>
-                    </label>
-                </div>
+                    <div>
+                        <label htmlFor="password">password
+                            <input type="password" name="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} required></input>
+                        </label>
 
-                <div>
-                    <label htmlFor="password">password
-                        <input type="password" name="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} required></input>
-                    </label>
+                    </div>
+                    <span>{loginStatus}</span>
 
-                </div>
-                <span>{loginStatus}</span>
-
-                <button type="submit">login</button>
-            </form>
-        </>
-    )
+                    <button type="submit">login</button>
+                </form>
+            </>
+        )
+    }
 }
