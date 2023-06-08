973250cb2ff7bc09d3d1eccdb9c3233faca34dfe
Mauritz Marais <mauritz303@gmail.com>
2023-06-08 17:23:33 +0200

    implemented function to fetch all posts, and state to display them

--- src/components/posts.jsx
+++ src/components/posts.jsx
@@ -1,16 +1,45 @@
 import { Link } from "react-router-dom"
+import { useEffect } from "react";
+import { useState } from "react";
 
 export default function Posts({ isAuthenticated }) {
+    const [posts, setPosts] = useState([]);
 
-    // fetch posts*
+    // fetch all posts function and update state
+    async function getPosts() {
+        try {
+            const response = await fetch('http://localhost:3001/admin/posts', { method: 'GET', credentials: 'include' });
+
+            const allPosts = await response.json();
+
+            setPosts([...allPosts]);
+        }
+        catch (error) {
+            console.log(error);
+        }
+    };
+
+    // run function 
+    useEffect(() => {
+        getPosts();
+    }, []);
+
+    // render list of posts
+    const postsDisplay = posts.map(post => {
+        return <div key={post.id}>
+            <p >{post.title} by {post.author} posted on {post.date}</p>
+        </div>
+    });
 
     // logged in?
     if (isAuthenticated) {
+
         return (
             <>
                 <h2>Posts</h2>
                 <Link to='/posts/new'>New post</Link>
-                <p>Edit</p>
+                <h3>Edit post:</h3>
+                <div>{postsDisplay}</div>
             </>
         )
     } else {
@@ -20,5 +49,4 @@ export default function Posts({ isAuthenticated }) {
             </>
         )
     }
-
-}
+};
