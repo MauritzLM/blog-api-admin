0c5c2f339229c1b21c8e30d96c8752f06434f1e6
Mauritz Marais <mauritz303@gmail.com>
2023-06-08 17:22:30 +0200

    implemented conditional rendering of header links based on authenticated state

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,19 +1,33 @@
 
 import { Link } from "react-router-dom"
 
-export default function Header() {
-    return (
-        <>
+export default function Header({ isAuthenticated }) {
+    if (isAuthenticated) {
+        return (
+            <>
+                <header>
+                    <nav>
+                        <ul>
+                            <li><Link to="/">Home</Link></li>
+                            <li><Link to="/login">Login</Link></li>
+                            <li><Link to="/signup">Sign up</Link></li>
+                            <li><Link to="/posts">Posts</Link></li>
+                        </ul>
+                    </nav>
+                </header>
+            </>
+        )
+    } else {
+        return (
             <header>
                 <nav>
                     <ul>
                         <li><Link to="/">Home</Link></li>
                         <li><Link to="/login">Login</Link></li>
                         <li><Link to="/signup">Sign up</Link></li>
-                        <li><Link to="/posts">Posts</Link></li>
                     </ul>
                 </nav>
             </header>
-        </>
-    )
-}
+        )
+    }
+};
