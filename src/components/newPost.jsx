2e44e30940a78d94924159d925ce8b01fd55dcc8
Mauritz Marais <mauritz303@gmail.com>
2023-06-08 17:24:21 +0200

    added error display to form

--- src/components/newPost.jsx
+++ src/components/newPost.jsx
@@ -4,7 +4,10 @@ import { Link } from "react-router-dom";
 export default function NewPost({ isAuthenticated }) {
     const [postCreated, setPostCreated] = useState(false);
 
-    // error state variables*
+    // error state variables
+    const [titleError, setTitleError] = useState('');
+    const [authorError, setAuthorError] = useState('');
+    const [contentError, setContentError] = useState('');
 
     // create post handle function*
     async function handleNewPost(event) {
@@ -15,7 +18,7 @@ export default function NewPost({ isAuthenticated }) {
             const form = event.target;
             const formData = new FormData(form);
 
-            // fetch request with credentials*
+            // fetch request with credentials
             const response = await fetch('http://localhost:3001/admin/posts/new', {
                 method: form.method,
                 credentials: "include",
@@ -24,27 +27,43 @@ export default function NewPost({ isAuthenticated }) {
 
             const data = await response.json();
 
-            // if errors display errors*
+            // if errors display errors
             if (data.errors) {
                 data.errors.forEach(err => {
-                    console.log(err);
-                    return;
+                    if (err.path === 'title') {
+                        setTitleError(err.msg);
+                    }
+                    if (err.path === 'author') {
+                        setAuthorError(err.msg);
+                    }
+                    if (err.path === 'postContent') {
+                        setContentError(err.msg);
+                    }
+
                 });
+                return;
             }
 
-            // if
-            console.log(data);
+            // if succcessful
+            setPostCreated(true);
 
         }
         catch (error) {
             console.log(error)
         }
-
     }
+
+    // change post created state
+    function handlePostCreatedChange() {
+        setPostCreated(false);
+    }
+
+    // conditional rendering
     if (postCreated) {
         return (
             <>
                 <h2>Post created</h2>
+                <button onClick={() => handlePostCreatedChange()}>Create another post</button>
                 <Link to="/posts">back to posts</Link>
             </>
         )
@@ -58,14 +77,18 @@ export default function NewPost({ isAuthenticated }) {
                 <form onSubmit={(e) => { handleNewPost(e) }} method="post">
                     <label htmlFor="title">Title
                         <input type="text" name="title" id="title" />
+                        <span>{titleError}</span>
                     </label>
 
                     <label htmlFor="author">Author
                         <input type="text" name="author" id="author" />
+                        <span>{authorError}</span>
                     </label>
 
-                    <label htmlFor="postContent"></label>
-                    <textarea name="postContent" id="postContent" cols="30" rows="10"></textarea>
+                    <label htmlFor="postContent">Post
+                        <textarea name="postContent" id="postContent" cols="30" rows="10" />
+                        <span>{contentError}</span>
+                    </label>
 
                     <button type="submit">submit</button>
                 </form>
