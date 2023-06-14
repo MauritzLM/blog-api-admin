e3478c0abea81ce411dff11f5f79a004b5a590a5
Mauritz Marais <mauritz303@gmail.com>
2023-06-14 15:58:26 +0200

    added form validation errors and date formatting

--- src/components/postComments.jsx
+++ src/components/postComments.jsx
@@ -1,31 +1,44 @@
 import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import dayjs from "dayjs";
+const relativeTime = require('dayjs/plugin/relativeTime');
+dayjs.extend(relativeTime);
 
 export default function Comments({ isAuthenticated }) {
 
+    // post id
     const { id } = useParams();
-    const [title, setTitle] = useState('');
-    const [author, setAuthor] = useState('');
-    const [comments, setComments] = useState([]);
+
+    // post object
+    const [post, setPost] = useState({});
+
+    // form values
+    const [commentFormValues, setCommentFormValues] = useState({ name: '', body: '' });
+
+    // validation errors
+    const [commentAuthorError, setCommentAuthorError] = useState('');
+    const [commentBodyError, setCommentBodyError] = useState('');
+
 
     // fetch post
     async function getComments() {
 
         try {
-            // fetch request* (better to get post or just comments? - need post title/author) 
-            const response = await fetch(`http://localhost:3001/admin/posts/${id}/comments`, {
+            // fetch request to get post
+            const response = await fetch(`http://localhost:3001/admin/posts/${id}`, {
                 method: 'GET',
                 credentials: "include",
             });
             // if errors*
-            const post = await response.json();
+            const postData = await response.json();
 
+            if (postData.error) {
+                console.log(postData.error.msg);
+            };
 
-            // update comments state* (better way to do it?)
-            setComments([...post.comments]);
-            setTitle(post.title);
-            setAuthor(post.author);
+            // update state
+            setPost({ ...postData });
         }
         catch (error) {
             console.log(error)
@@ -51,16 +64,32 @@ export default function Comments({ isAuthenticated }) {
 
             const data = await response.json();
 
-            // errors*
+            // validation errors
             if (data.errors) {
                 data.errors.forEach(err => {
-                    console.log(err.msg);
+                    if (err.path === "commentAuthor") {
+                        setCommentAuthorError(err.msg);
+                    }
+                    if (err.path === "commentBody") {
+                        setCommentBodyError(err.msg);
+                    }
                 });
                 return;
             };
 
-            // success* (comment added message?)
-            console.log(data)
+            // update form input state (to reset form values)
+            setCommentFormValues({ name: '', body: '' });
+
+            // fetch updated post
+            const fetchPost = await fetch(`http://localhost:3001/admin/posts/${id}`, {
+                method: 'GET',
+                credentials: "include",
+            });
+
+            const updatedPost = await fetchPost.json();
+
+            // update state
+            setPost({ ...updatedPost });
 
         }
         catch (error) {
@@ -68,28 +97,26 @@ export default function Comments({ isAuthenticated }) {
         }
     };
 
-    // get all comments on mount (how to update when new comment posted?)*
+
+    // get all comments
     useEffect(() => {
         getComments();
     }, []);
 
-    // update comment function*(edit comment component - to edit or remove)
-
-    // delete comment function*
 
     // create list of comments
     if (isAuthenticated) {
         return (
             <>
-                <h2>Viewing comments of {title} by {author}</h2>
+                <h2>Viewing comments of {post.title} by {post.author}</h2>
                 <Link to={`/posts/${id}`}>back to post</Link>
                 <div className="comment-container">
-                    {comments.map(comment => {
-                        return <CommentCard comment={comment} />
+                    {post.comments?.map(comment => {
+                        return <CommentCard comment={comment} key={comment._id} postid={id} />
                     })}
                 </div>
                 <h3>Add new comment</h3>
-                <CreateCommentForm handleCommentCreate={handleCommentCreate} />
+                <CreateCommentForm handleCommentCreate={handleCommentCreate} commentFormValues={commentFormValues} setCommentFormValues={setCommentFormValues} commentAuthorError={commentAuthorError} commentBodyError={commentBodyError} />
             </>
         )
     }
@@ -97,16 +124,19 @@ export default function Comments({ isAuthenticated }) {
 
 
 // create comment form
-function CreateCommentForm({ handleCommentCreate }) {
+function CreateCommentForm({ handleCommentCreate, commentFormValues, setCommentFormValues, commentAuthorError, commentBodyError }) {
+
     return (
         <>
             <form onSubmit={(e) => handleCommentCreate(e)} method="post" noValidate>
                 <label htmlFor="commentAuthor">name
-                    <input type="text" name="commentAuthor" id="commentAuthor"></input>
+                    <input type="text" name="commentAuthor" id="commentAuthor" value={commentFormValues.name} onChange={(e) => setCommentFormValues({ ...commentFormValues, name: e.target.value })}></input>
+                    <span>{!commentFormValues.name ? commentAuthorError : ""}</span>
                 </label>
 
                 <label htmlFor="commentBody">
-                    <textarea name="commentBody" id="commentBody" cols="30" rows="10" />
+                    <textarea name="commentBody" id="commentBody" cols="30" rows="10" value={commentFormValues.body} onChange={(e) => setCommentFormValues({ ...commentFormValues, body: e.target.value })} />
+                    <span>{!commentFormValues.body ? commentBodyError : ""}</span>
                 </label>
 
                 <button type="submit">Post comment</button>
@@ -116,14 +146,14 @@ function CreateCommentForm({ handleCommentCreate }) {
 };
 
 // comment card*
-function CommentCard({ comment }) {
+function CommentCard({ comment, postid }) {
     return (
         <>
             <div className="comment-card">
                 <p>{comment.body}</p>
                 <p>{comment.author}</p>
-                <p>{comment.timestamp}</p>
-                {/* <Link to={`/posts/${id}/comments/${comment._id}`}>edit</Link> */}
+                <p>{dayjs(comment.timestamp).fromNow()}</p>
+                <Link to={`/posts/${postid}/comments/${comment._id}`}>edit</Link>
             </div>
         </>
     )
